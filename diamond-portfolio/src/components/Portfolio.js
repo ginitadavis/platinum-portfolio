26a4e3ff968a4f1f0579ce78082b4cf86c0d4668
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-05-21 15:30:11 -0400

    Added github and deployment links to cards

--- diamond-portfolio/src/components/Portfolio.js
+++ diamond-portfolio/src/components/Portfolio.js
@@ -1,78 +1,115 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Content.css';
 import { Image } from 'antd';
 import codeQuiz from '../images/code-quiz.png';
 import eCommerce from '../images/e-commerce-back-end.png';
 import employeeTracker from '../images/employee-tracker.png';
 import localFinds from '../images/Project1-local-finds.png';
-import floridaSprings from '../images/Project2-Florida-Springs.png';
+import floridaSprings from '../images/Project2-Florida-Springs.jpg';
 import socialNetwork from '../images/social-network-api.png';
 import teamGenerator from '../images/team-generator.png';
 import weatherForecast from '../images/weather-forecast.png';
 import workDayScheduler from '../images/work-day-scheduler.png';
 import imagePlaceholder from '../images/image-placeholder.png';
+import { RocketOutlined, GithubOutlined, } from '@ant-design/icons';
+import { Card } from 'antd';
 
 function Portfolio() {
 
     return (
         <>
             <h1 className='title'>Portfolio</h1>
-            <div className='imageContainer'>
-                {/* <Image.PreviewGroup
-                    preview={{
-                        onChange: (current, prev) =>
-                            console.log(`current index: ${current}, prev index: ${prev}`),
-                    }}
-                > */}
-                <a href='https://github.com/ginitadavis/Code-Quiz' target='_blank' rel='noopener noreferrer'>
-                    <Image className='portfolioImage' width={200} height={200} src={codeQuiz} />
-                </a>
 
-                <a href='https://github.com/ginitadavis/Word-Day-Scheduler' target='_blank' rel='noopener noreferrer'>
-                    <Image className='portfolioImage' src={workDayScheduler} />
-                </a>
 
-                <a href='https://github.com/ginitadavis/Weather-Forecast' target='_blank' rel='noopener noreferrer'>
-                    <Image className='portfolioImage' src={weatherForecast} />
-                </a>
 
-                <a href='https://github.com/ginitadavis/Team-Profile-Generator' target='_blank' rel='noopener noreferrer'>
+            <div className='imageContainer' style={{ display: 'flex', alignItems: 'center' }}>
+
+                {/* Code quiz card */}
+                <Card style={{ width: 300, margin: 10 }}>
+                    <div>
+                        <Image className='portfolioImage' width={200} height={200} src={codeQuiz} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <a href="https://ginitadavis.github.io/Code-Quiz/" target='_blank'> <RocketOutlined style={{ marginRight: 8 }} /> Deployment </a>
+                            <a href="https://github.com/ginitadavis/Code-Quiz" target='_blank'> <GithubOutlined style={{ marginRight: 8, marginLeft: 8 }} /> GitHub </a>
+                        </div>
+                    </div>
+                </Card>
+
+                {/* Work Day Scheduler */}
+                <Card style={{ width: 300, margin: 10 }}>
+                    <div>
+                        <Image className='portfolioImage' width={200} height={200} src={workDayScheduler} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <a href="https://ginitadavis.github.io/Word-Day-Scheduler/" target='_blank'> <RocketOutlined style={{ marginRight: 8 }} /> Deployment </a>
+                            <a href="https://github.com/ginitadavis/Word-Day-Scheduler" target='_blank'> <GithubOutlined style={{ marginRight: 8, marginLeft: 8 }} /> GitHub </a>
+                        </div>
+                    </div>
+                </Card>
+
+                {/* Weather Forecast */}
+                <Card style={{ width: 300, margin: 10 }}>
+                    <div>
+                        <Image className='portfolioImage' width={200} height={200} src={weatherForecast} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <a href="https://ginitadavis.github.io/Weather-Forecast/" target='_blank'> <RocketOutlined style={{ marginRight: 8 }} /> Deployment </a>
+                            <a href="https://github.com/ginitadavis/Weather-Forecast" target='_blank'> <GithubOutlined style={{ marginRight: 8, marginLeft: 8 }} /> GitHub </a>
+                        </div>
+                    </div>
+                </Card>
+
+                {/* Local Finds */}
+                <Card style={{ width: 300, margin: 10 }}>
+                    <div>
+                        <Image className='portfolioImage' width={200} height={200} src={localFinds} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <a href="https://jpshirey5.github.io/Local_Finds/" target='_blank'> <RocketOutlined style={{ marginRight: 8 }} /> Deployment </a>
+                            <a href="https://github.com/Jpshirey5/Local_Finds" target='_blank'> <GithubOutlined style={{ marginRight: 8, marginLeft: 8 }} /> GitHub </a>
+                        </div>
+                    </div>
+                </Card>
+
+                {/* Employee Tracker */}
+                <Card style={{ width: 300, margin: 10 }}>
+                    <div>
+                        <Image className='portfolioImage' width={200} height={200} src={employeeTracker} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <a href="https://drive.google.com/file/d/1aTD0u971Nic2wWYyTswJWGzdkmmq4hN4/view" target='_blank'> <RocketOutlined style={{ marginRight: 8 }} /> Deployment </a>
+                            <a href="https://github.com/ginitadavis/Employee-Tracker" target='_blank'> <GithubOutlined style={{ marginRight: 8, marginLeft: 8 }} /> GitHub </a>
+                        </div>
+                    </div>
+                </Card>
+
+                {/* Florida Springs */}
+                <Card style={{ width: 300, margin: 10 }}>
+                    <div>
+                        <Image className='portfolioImage' width={200} height={200} src={floridaSprings} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <a href="https://floridasprings.herokuapp.com/" target='_blank'> <RocketOutlined style={{ marginRight: 8 }} /> Deployment </a>
+                            <a href="https://github.com/hmccarthy1/Trip-Planner" target='_blank'> <GithubOutlined style={{ marginRight: 8, marginLeft: 8 }} /> GitHub </a>
+                        </div>
+                    </div>
+                </Card>
+
+                {/* Team Generator */}
+                {/* <a href='https://github.com/ginitadavis/Team-Profile-Generator' target='_blank' rel='noopener noreferrer'>
                     <Image className='portfolioImage' src={teamGenerator} />
-                </a>
+                </a> */}
 
-                <a href='https://github.com/ginitadavis/Employee-Tracker' target='_blank' rel='noopener noreferrer'>
-                    <Image className='portfolioImage' src={employeeTracker} />
-                </a>
-
-                <a href='https://github.com/Jpshirey5/Local_Finds' target='_blank' rel='noopener noreferrer'>
-                    <Image className='portfolioImage' src={localFinds} />
-                </a>
-
-                <a href='https://github.com/ginitadavis/E_commerce_Back_End' target='_blank' rel='noopener noreferrer'>
+                {/* E-Commerce */}
+                {/* <a href='https://github.com/ginitadavis/E_commerce_Back_End' target='_blank' rel='noopener noreferrer'>
                     <Image className='portfolioImage' src={eCommerce} />
-                </a>
+                </a> */}
 
-                <a href='https://github.com/ginitadavis/Social-Network-API' target='_blank' rel='noopener noreferrer'>
+                {/* Social Network */}
+                {/* <a href='https://github.com/ginitadavis/Social-Network-API' target='_blank' rel='noopener noreferrer'>
                     <Image className='portfolioImage' src={socialNetwork} />
-                </a>
+                </a> */}
 
-                <a href='https://github.com/hmccarthy1/Trip-Planner' target='_blank' rel='noopener noreferrer'>
-                    <Image className='portfolioImage' src={floridaSprings} />
-                </a>
-
-                <a href='https://github.com/ginitadavis/Tech-Blog' target='_blank' rel='noopener noreferrer'>
+                {/* Tech Blog in progress */}
+                {/* <a href='https://github.com/ginitadavis/Tech-Blog' target='_blank' rel='noopener noreferrer'>
                     <Image className='portfolioImage' src={imagePlaceholder} />
-                </a>
-                {/* <Image className='portfolioImage' src={codeQuiz} /> */}
+                </a> */}
 
-
-
-
-
-
-
-
-                {/* </Image.PreviewGroup> */}
             </div>
         </>
     );
