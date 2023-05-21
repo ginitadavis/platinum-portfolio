ac1775f4d0be7f7a295d9e50bddaf14d18f90eab
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-05-20 21:22:22 -0400

    Finished Resume page

--- diamond-portfolio/src/components/Resume.js
+++ diamond-portfolio/src/components/Resume.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import '../styles/Content.css';
+import { Typography } from 'antd';
+const { Link } = Typography;
 
 function Resume() {
     return (
         <>
-            <h1>Resume</h1>
+            <h1 className='title'>Resume</h1>
+            <div className='resumeInformation'>
+                <a
+                    href={process.env.PUBLIC_URL + '/Gina-Davis-Resume.pdf'}
+                    download='Gina-Davis-Resume.pdf'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Download my Resume
+                </a>
+                <h2 className='subTitle'>front-end Proficiencies</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>JQuery</li>
+                    <li>responsive design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+                <h2 className='subTitle'>back-end Proficiencies</h2>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                </ul>
+            </div>
+
         </>
     )
 }
